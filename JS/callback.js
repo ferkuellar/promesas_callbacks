e0528e600caf1443dda2c4f95d1b8b77ced4e762
Fernando Cuellar <fercuellar@gmail.com>
2023-04-01 10:28:52 -0600

    Inico de practica

---
+++ JS/callback.js
@@ -0,0 +1,30 @@
+// console.log('saludos');
+// console.log('como estas?');
+// console.log('adios');
+
+
+// callback Hell se anidan muchos callback
+// const saludar = () => {
+//     console.log('Hola');
+// };
+
+// const preguntar = (callback, callback2) => {
+//     setTimeout(() =>{
+//         console.log('como estas?');
+//         callback(callback2);
+//     }, 3000);
+// };
+
+// const despedir = (callback) => {
+//     console.log('adios');
+//     callback();
+// };
+
+// const funcion2 = () => {
+//     console.log('funcion 2')
+// }
+
+// saludar();
+// preguntar(despedir, funcion2);
+// despedir();
+
---
+++ JS/ejercicio1.js
@@ -0,0 +1,15 @@
+
+function saludar(str, callback) {
+    var saludo = "¡Hola " + str.toUpperCase() + "!";
+    setTimeout(function() {
+        callback(saludo);
+    }, 4000);
+};
+
+function imprimirSaludo(saludo) {
+    console.log(saludo);
+}
+
+saludar('Fernando', imprimirSaludo);
+
+console.log("final");
